1541b3e1d5d1ac4c5312d1ba0e6dca6f1a414e70
naker90 <AntonioJesusSG96@gmail.com>
2017-05-29 14:06:53 +0100

    refactor

--- src/UpdateNumber.java
+++ src/UpdateNumber.java
@@ -1,4 +1,3 @@
-import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
@@ -9,17 +8,12 @@ public class UpdateNumber {
 
         Method[] methods = typeClass.getMethods();
         for (Method method : methods) {
-            injectResult(number, method, typeClass);
+            updateResult(number, method);
         }
     }
 
-    private void injectResult(Object object, Method method, Class<?> typeClass) {
+    private void updateResult(Object object, Method method) {
         if (isMethodValid(method)) try {
-                /*
-                Field var = object.getClass().getField("number");
-                int value = (int) var.get(object);
-                var.setInt(object, value + 5);
-                */
             int result = (Integer) method.invoke(object, (Object[]) null);
             System.out.println(result + method.getAnnotation(AddToResult.class).increment());
         } catch (IllegalAccessException | InvocationTargetException e) {
@@ -28,8 +22,6 @@ public class UpdateNumber {
     }
 
     private boolean isMethodValid(Method method) {
-        System.out.println(method.getAnnotation(AddToResult.class));
-        System.out.println(method.getReturnType().toString().equals("int"));
         return method.getAnnotation(AddToResult.class) != null
                 && method.getReturnType().toString().equals("int");
     }
